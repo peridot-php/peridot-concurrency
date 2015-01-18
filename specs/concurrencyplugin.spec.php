9911ad5493539b56b629af8b6e46324a9705c12d
Brian Scaturro <scaturrob@gmail.com>
2015-01-17 21:39:58 -0500

    store a reference to input on peridot.execute

--- specs/concurrencyplugin.spec.php
+++ specs/concurrencyplugin.spec.php
@@ -2,6 +2,7 @@
 use Peridot\Concurrency\ConcurrencyPlugin;
 use Peridot\Console\InputDefinition;
 use Peridot\Console\Environment;
+use Symfony\Component\Console\Input\StringInput;
 use Evenement\EventEmitter;
 
 describe('ConcurrencyPlugin', function () {
@@ -19,6 +20,14 @@ describe('ConcurrencyPlugin', function () {
         it('should register a --concurrent option', function () {
             $this->emitter->emit('peridot.start', [$this->environment]);
             expect($this->definition->hasOption('concurrent'))->to->be->true;
+        });
+    });
+
+    context('when peridot.execute event is emitted', function () {
+        it('should store a reference to input object' , function () {
+            $input = new StringInput('');
+            $this->emitter->emit('peridot.execute', [$input]);
+            expect($this->plugin->getInput())->to->equal($input);
         });
     });
 });
--- src/ConcurrencyPlugin.php
+++ src/ConcurrencyPlugin.php
@@ -4,6 +4,7 @@ namespace Peridot\Concurrency;
 use Peridot\Console\Environment;
 use Evenement\EventEmitterInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
 
 class ConcurrencyPlugin
 {
@@ -13,22 +14,48 @@ class ConcurrencyPlugin
     private $emitter;
 
     /**
+     * @var InputInterface
+     */
+    private $input;
+
+    /**
      * @param EventEmitterInterface $emitter
      */
     public function __construct(EventEmitterInterface $emitter)
     {
         $this->emitter = $emitter;
         $this->emitter->on('peridot.start', [$this, 'onPeridotStart']);
+        $this->emitter->on('peridot.execute', [$this, 'onPeridotExecute']);
     }
 
     /**
      * Registers a --concurrent option with peridot
      *
      * @param Environment $env
+     * @return void
      */
     public function onPeridotStart(Environment $env)
     {
         $definition = $env->getDefinition();
         $definition->option('concurrent', null, InputOption::VALUE_NONE, "run specs concurrently");
+    }
+
+    /**
+     * Stores a reference to the peridot input interface.
+     * @return void
+     */
+    public function onPeridotExecute(InputInterface $input)
+    {
+        $this->input = $input;
+    }
+
+    /**
+     * Returns the store input interface reference.
+     *
+     * @return InputInterface
+     */
+    public function getInput()
+    {
+        return $this->input;
     }
 }
