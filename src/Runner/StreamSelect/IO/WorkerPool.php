3b036583a02ffefe3755f05f543ff0b6f5b89663
Brian Scaturro <scaturrob@gmail.com>
2015-01-22 12:47:24 -0500

    pull runner process management into pool

---
+++ src/Runner/StreamSelect/IO/WorkerPool.php
@@ -0,0 +1,120 @@
+<?php
+namespace Peridot\Concurrency\Runner\StreamSelect\IO;
+
+use Evenement\EventEmitterInterface;
+use Peridot\Concurrency\Configuration;
+use Peridot\Core\HasEventEmitterTrait;
+
+/**
+ * The WorkerPool manages open worker processes.
+ *
+ * @package Peridot\Concurrency\Runner\StreamSelect\IO
+ */
+class WorkerPool
+{
+    use HasEventEmitterTrait;
+
+    /**
+     * @var \Peridot\Concurrency\Configuration
+     */
+    protected $configuration;
+
+    /**
+     * @var int
+     */
+    protected $pending = 0;
+
+    /**
+     * @var array
+     */
+    protected $workers = [];
+
+    /**
+     * @var ResourceOpenInterface
+     */
+    protected $resourceOpen;
+
+    /**
+     * @param Configuration $configuration
+     * @param EventEmitterInterface $eventEmitter
+     * @param ResourceOpenInterface $resourceOpen
+     */
+    public function __construct(
+        Configuration $configuration,
+        EventEmitterInterface $eventEmitter,
+        ResourceOpenInterface $resourceOpen = null
+    ) {
+        $this->configuration = $configuration;
+        $this->eventEmitter = $eventEmitter;
+        $this->resourceOpen = $resourceOpen;
+        $this->eventEmitter->on('peridot.concurrency.loadstart', [$this, 'setPending']);
+    }
+
+    /**
+     * Start worker processes, attaching worker processes
+     * to fill the number of configured processes.
+     *
+     * @return void
+     */
+    public function startWorkers()
+    {
+        $processes = $this->configuration->getProcesses();
+        for ($i = 0; $i < $processes; $i++) {
+            $exec = __DIR__ . '/select-runner.php';
+            $worker = new Worker("php $exec", $this->eventEmitter, $this->resourceOpen);
+            $this->attach($worker);
+        }
+    }
+
+    /**
+     * Attach a worker to the WorkerPool and start
+     * it if it is not already started.
+     *
+     * @return bool
+     */
+    public function attach(WorkerInterface $worker)
+    {
+        if (sizeof($this->workers) === $this->configuration->getProcesses()) {
+            return false;
+        }
+
+        $this->workers[] = $worker;
+
+        if (! $worker->isStarted()) {
+            $worker->start();
+        }
+
+        return true;
+    }
+
+    /**
+     * Get all workers attached to the runner.
+     *
+     * @return array
+     */
+    public function getWorkers()
+    {
+        return $this->workers;
+    }
+
+    /**
+     * Set the number of pending tests.
+     *
+     * @param int $pending
+     * @return void
+     */
+    public function setPending($pending)
+    {
+        $this->pending = $pending;
+    }
+
+    /**
+     * Get the number of pending tests.
+     *
+     * @return int
+     */
+    public function getPending()
+    {
+        return $this->pending;
+    }
+} 
--- src/Runner/StreamSelect/StreamSelectRunner.php
+++ src/Runner/StreamSelect/StreamSelectRunner.php
@@ -19,43 +19,21 @@ class StreamSelectRunner implements RunnerInterface
     use HasEventEmitterTrait;
 
     /**
-     * @var Configuration
+     * @var IO\WorkerPool
      */
-    protected $config;
+    protected $pool;
 
     /**
-     * @var EventEmitterInterface
-     */
-    protected $emitter;
-
-    /**
-     * @var int
-     */
-    protected $pending = 0;
-
-    /**
-     * @var array
-     */
-    protected $workers = [];
-
-    /**
-     * @var IO\ResourceOpenInterface
-     */
-    protected $resourceOpen;
-
-    /**
-     * @param Configuration $config
      * @param EventEmitterInterface $emitter
+     * @param IO\WorkerPool $pool
      */
     public function __construct(
-        Configuration $config,
         EventEmitterInterface $emitter,
-        IO\ResourceOpenInterface $resourceOpen = null
+        IO\WorkerPool $pool
     ) {
-        $this->config = $config;
         $this->eventEmitter = $emitter;
-        $this->resourceOpen = $resourceOpen ?: new IO\ProcOpen();
-        $this->listen();
+        $this->pool = $pool;
+        $this->eventEmitter->on('peridot.concurrency.suiteloading', [$this, 'onSuiteLoading']);
     }
 
     /**
@@ -66,64 +44,7 @@ class StreamSelectRunner implements RunnerInterface
      */
     public function run(TestResult $result)
     {
-        $this->startWorkers();
-    }
-
-    /**
-     * Attach a worker to the StreamSelectRunner and start
-     * it if it is not already started.
-     *
-     * @return bool
-     */
-    public function attach(IO\WorkerInterface $worker)
-    {
-        if (sizeof($this->workers) === $this->config->getProcesses()) {
-            return false;
-        }
-
-        $this->workers[] = $worker;
-
-        if (! $worker->isStarted()) {
-            $worker->start();
-        }
-
-        return true;
-    }
-
-    /**
-     * Start worker processes.
-     *
-     * @return void 
-     */
-    public function startWorkers()
-    {
-        $processes = $this->config->getProcesses();
-        for ($i = 0; $i < $processes; $i++) {
-            $exec = __DIR__ . '/select-runner.php';
-            $worker = new IO\Worker("php $exec", $this->eventEmitter, $this->resourceOpen);
-            $this->attach($worker);
-        }
-    }
-
-    /**
-     * Set the number of pending tests.
-     *
-     * @param int $pending
-     * @return void
-     */
-    public function setPending($pending)
-    {
-        $this->pending = $pending;
-    }
-
-    /**
-     * Get the number of pending tests.
-     *
-     * @return int
-     */
-    public function getPending()
-    {
-        return $this->pending;
+        $this->pool->startWorkers();
     }
 
     /**
@@ -134,26 +55,5 @@ class StreamSelectRunner implements RunnerInterface
      */
     public function onSuiteLoading($suitePath)
     {
-    }
-
-    /**
-     * Get all workers attached to the runner.
-     *
-     * @return array
-     */
-    public function getWorkers()
-    {
-        return $this->workers;
-    }
-
-    /**
-     * Setup event listeners.
-     *
-     * @return void
-     */
-    protected function listen()
-    {
-        $this->eventEmitter->on('peridot.concurrency.loadstart', [$this, 'setPending']);
-        $this->eventEmitter->on('peridot.concurrency.suiteloading', [$this, 'onSuiteLoading']);
     }
 }
