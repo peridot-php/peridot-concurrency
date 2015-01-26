d238f71d69cebf86bc5e2cb03615212326aebe8d
Brian Scaturro <scaturrob@gmail.com>
2015-01-26 12:07:01 -0500

    create message namespace for stream select runner

--- specs/runners/stream-select/message/read-message.spec.php
+++ specs/runners/stream-select/message/read-message.spec.php
@@ -1,19 +1,21 @@
 <?php
-describe('Message', function () {
+use Peridot\Concurrency\Runner\StreamSelect\Message\ReadableMessage;
+
+describe('ReadableMessage', function () {
     beforeEach(function () {
         $this->resource = tmpfile();
     });
 
     describe('->receive()', function () {
         it('should read from a stream', function () {
-            $message = new Message($this->resource);
+            $message = new ReadableMessage($this->resource);
             fwrite($this->resource, "hello world");
             $message->receive();
             expect($message->getContent())->to->equal('hello world');
         });
 
         it('should read until no more content', function () {
-            $message = new Message($this->resource, 1);
+            $message = new ReadableMessage($this->resource, 1);
             fwrite($this->resource, "hello world");
             $message->receive();
             expect($message->getContent())->to->equal('hello world');
@@ -22,78 +24,8 @@ describe('Message', function () {
 
     describe('->getResource()', function() {
         it('should return the underlying resource', function () {
-            $message = new Message($this->resource);
+            $message = new ReadableMessage($this->resource);
             expect($message->getResource())->to->equal($this->resource);
         });
     });
 });
-
-class Message
-{
-    /**
-     * @var resource
-     */
-    protected $resource;
-
-    /**
-     * @var int
-     */
-    protected $chunkSize;
-
-    /**
-     * @var string
-     */
-    protected $content;
-
-    /**
-     * @var int
-     */
-    protected $offset;
-
-    /**
-     * @param resource $resource
-     * @param int $chunkSize
-     */
-    public function __construct($resource, $chunkSize = 4096)
-    {
-        $this->resource = $resource;
-        $this->chunkSize = $chunkSize;
-        $this->content = '';
-        $this->offset = ftell($resource);
-    }
-
-    /**
-     * Read data from a stream. Currently read content
-     * can be fetched with getContent().
-     *
-     * @return void
-     */
-    public function receive()
-    {
-        fseek($this->resource, $this->offset);
-        while ($content = fread($this->resource, $this->chunkSize)) {
-            $this->content .= $content;
-        }
-        $this->offset = ftell($this->resource);
-    }
-
-    /**
-     * Get content.
-     *
-     * @return string
-     */
-    public function getContent()
-    {
-        return $this->content;
-    }
-
-    /**
-     * Get the resource.
-     *
-     * @return resource
-     */
-    public function getResource()
-    {
-        return $this->resource;
-    }
-}
---
+++ src/Runner/StreamSelect/Message/ReadableMessage.php
@@ -0,0 +1,81 @@
+<?php
+namespace Peridot\Concurrency\Runner\StreamSelect\Message;
+
+use Evenement\EventEmitterTrait;
+
+/**
+ * A ReadableMessage is meant to receive
+ * data from a stream.
+ *
+ * @package Peridot\Concurrency\Runner\StreamSelect\Message
+ */
+class ReadableMessage
+{
+    use EventEmitterTrait;
+    /**
+     * @var resource
+     */
+    protected $resource;
+
+    /**
+     * @var int
+     */
+    protected $chunkSize;
+
+    /**
+     * @var string
+     */
+    protected $content;
+
+    /**
+     * @var int
+     */
+    protected $offset;
+
+    /**
+     * @param resource $resource
+     * @param int $chunkSize
+     */
+    public function __construct($resource, $chunkSize = 4096)
+    {
+        $this->resource = $resource;
+        $this->chunkSize = $chunkSize;
+        $this->content = '';
+        $this->offset = ftell($resource);
+    }
+
+    /**
+     * Read data from a stream. Currently read content
+     * can be fetched with getContent().
+     *
+     * @return void
+     */
+    public function receive()
+    {
+        fseek($this->resource, $this->offset);
+        while ($content = fread($this->resource, $this->chunkSize)) {
+            $this->content .= $content;
+        }
+        $this->offset = ftell($this->resource);
+    }
+
+    /**
+     * Get content.
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Get the resource.
+     *
+     * @return resource
+     */
+    public function getResource()
+    {
+        return $this->resource;
+    }
+} 
