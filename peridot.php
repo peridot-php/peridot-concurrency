8a728456ec00829e219d36466eb923505647c6c0
Brian Scaturro <scaturrob@gmail.com>
2015-02-09 19:20:30 -0500

    add code coverage plugins to dev

--- peridot.php
+++ peridot.php
@@ -3,15 +3,22 @@ use Evenement\EventEmitterInterface;
 use Peridot\Concurrency\ConcurrencyPlugin;
 use Peridot\Console\Environment;
 use Peridot\Plugin\Prophecy\ProphecyPlugin;
+use Peridot\Reporter\CodeCoverage\AbstractCodeCoverageReporter;
+use Peridot\Reporter\CodeCoverageReporters;
 use Peridot\Reporter\Dot\DotReporterPlugin;
 
 return function (EventEmitterInterface $emitter) {
     $prophecy = new ProphecyPlugin($emitter);
     $concurrency = new ConcurrencyPlugin($emitter);
     $dot = new DotReporterPlugin($emitter);
+    (new CodeCoverageReporters($emitter))->register();
 
     $emitter->on('peridot.start', function (Environment $env) {
         $definition = $env->getDefinition();
         $definition->getArgument('path')->setDefault('specs');
+    });
+
+    $emitter->on('code-coverage.start', function (AbstractCodeCoverageReporter $reporter) {
+        $reporter->addDirectoryToWhitelist(__DIR__ . '/src');
     });
 };
